d539d9e1f165ec8b373b317475162a530bacf394
kittichat <kittichat.ohm@gmail.com>
2020-11-02 16:08:55 +0700

    almost done

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('test1', views.test1, name='test1.html'),
+    path('test2', views.test2, name='test2.html'),
 ]
 
